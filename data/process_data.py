6b1cf88252057335e22576f745e29bdd4d34b802
gregory <gregory.marchesan@ecomp.ufsm.br>
2021-04-17 18:31:35 -0300

    feat: script to pre process updated and working

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,68 @@
 import sys
-
+import numpy as np
+import pandas as pd
+import sqlite3
+from tqdm import tqdm
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """
+    Loads data from the parameters and merge them in a dataframe
 
+    Parameters:
+    messages_filepath (string): filepath to the file containing messages
+    categories_filepath (string): filepath to the file containing categories
+
+    Returns:
+    df (dataframe): dataframe containing the loaded and merged information
+    """
+
+    df_messages = pd.read_csv(messages_filepath)
+    df_messages.drop('original', inplace=True, axis=1)
+    df_messages.set_index('id', inplace=True)
+    
+    df_categories = pd.read_csv(categories_filepath)
+    df_categories.set_index('id', inplace=True)
+
+    df = pd.concat([df_messages, df_categories], axis=1)
+
+    return df
 
 def clean_data(df):
-    pass
+    """
+    Cleans and transform data from the provided dataframe, transforming the 'categories' column to a multi-column values
+
+    Parameters:
+    df (dataframe): Dataframe containing messages and categories
+
+    Returns:
+    df (dataframe): Dataframe cleaned and transformed
+    """
+
+    # split the 'categories' column in multi-columns one-hot encoded
+    for index, row in tqdm(df.iterrows()):
+        for col in row['categories'].split(';'):
+            colname = col.split('-')[0]
+            value = col.split('-')[1]
+
+            df.loc[index, colname] = value
+
+    df.drop('categories', axis=1, inplace=True)
+
+    df['message'] = df['message'].str.lower()
+
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    """
+    Saves the df dataframe into a SQLite database usng the database filename provided
+
+    Parameters:
+    df (dataframe): A dataframe containing the information to be stored in the SQLite database
+    database_filename (string): The SQLite database filename
+    """
+    conn = sqlite3.connect(database_filename)
+    df.to_sql(database_filename, con = conn, if_exists='replace', index=False)
 
 
 def main():
