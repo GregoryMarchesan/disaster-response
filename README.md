160594e588b11d573ea1597f2333b17288961253
gregory <gregory.marchesan@ecomp.ufsm.br>
2021-04-20 21:22:24 -0300

    docs: readme updated

--- README.md
+++ README.md
@@ -1,5 +1,42 @@
 # Disaster Response Pipeline Project
 
+## Summary
+
+The project aims to use a NLP Machine Learning Pipeline in order to detect cases and 
+provide faster help when disasters occur. 
+The data was provided by Figure Eight and is available in the 'data' folder.
+
+## Folder organization
+
+The project is organized as follows:
+
+- app
+| - template
+| |- master.html  # main page of web app
+| |- go.html  # classification result page of web app
+|- run.py  # Flask file that runs app
+
+- data
+|- disaster_categories.csv  # data to process 
+|- disaster_messages.csv  # data to process
+|- process_data.py
+|- InsertDatabaseName.db   # database to save clean data to
+
+- models
+|- train_classifier.py
+|- classifier.pkl  # saved model 
+
+- README.md
+
+## Instalation
+
+The code runs in Python 3.7.3 and requires the following libraries:
+- Numpy (1.16.2)
+- Pandas (0.24.2)
+- Sklearn (0.20.3)
+- Plotly (4.14.3)
+- Tqdm (4.31.1)
+
 ### Instructions:
 1. Run the following commands in the project's root directory to set up your database and model.
 
@@ -12,3 +49,7 @@
     `python run.py`
 
 3. Go to http://0.0.0.0:3001/
+
+## Acknowledgements & Licensing
+
+The code provided here is free to use and it is under MIT Licensing.
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,165 @@
 import sys
+import numpy as np 
+import pandas as pd 
+from tqdm import tqdm 
 
+import sqlite3
+import pickle
+import joblib
+
+import nltk
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger'])
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import f1_score, precision_score, recall_score
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.base import BaseEstimator, TransformerMixin
+
+class GenreOneHotEncoding(BaseEstimator, TransformerMixin):
+
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, X):
+        return pd.get_dummies(X)
 
 def load_data(database_filepath):
-    pass
+    """
+    Loads the database from a SQLite file into the script and split it in X, y and category names
+
+    Parameters:
+    database_filename (string): name of the database
+
+    Returns:
+    X (numpy array): Array containing the X values (messages)
+    y (numpy array): Array containing the y values (one hot enconding of categories)
+    category_names (list): List containing the category names in the database
+    """
+    conn = sqlite3.connect(database_filepath)
+
+    # get a cursor
+    cur = conn.cursor()
+
+    # create the test table including project_id as a primary key
+    df = pd.read_sql("SELECT * FROM '{}'".format(database_filepath), con=conn)
+
+    conn.commit()
+    conn.close()
+
+    # genre_one_hot = pd.get_dummies(df["genre"])
+
+    # X = pd.concat([df["message"], genre_one_hot], axis=1).values
+    X = df[["message", "genre"]].values
+    y = df.drop(["message", "genre", "related"], axis=1).astype(int).values
+
+    category_names = df.drop(["message", "genre", "related"], axis=1).columns
+
+    return X, y, category_names
 
 
 def tokenize(text):
-    pass
+    """
+    Tokenize the text to further process in the ML algorithm
+
+    Parameters:
+    text (string): the text from each message
+
+    Returns:
+    clean_tokens (list): The list of tokens processed
+    """
+
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
 
 
 def build_model():
-    pass
+    """
+    Using grid search, builds the model to classify the messages
+
+    Returns:
+    model (): The trained model over the data
+    """
+
+    text_pipeline = Pipeline([
+                              ('vect', CountVectorizer(tokenizer=tokenize)),
+                              ('tfidf', TfidfTransformer())
+                            ])
+
+    genre_pipeline = Pipeline([
+                               ('genre', GenreOneHotEncoding())
+                             ])
+
+    preprocessor = ColumnTransformer(transformers=[('text_pipeline', text_pipeline, 0),
+                                                   ('num', genre_pipeline, 1)
+                                                  ])
+
+    pipeline = Pipeline([
+        ('preprocessor', preprocessor),
+        ('clf', RandomForestClassifier())
+    ])
+
+    parameters = {
+        'preprocessor__text_pipeline__vect__ngram_range': ((1, 1), (1, 2)),
+        'preprocessor__text_pipeline__vect__max_df': (0.5, 0.75, 1.0),
+        'preprocessor__text_pipeline__vect__max_features': (None, 5000, 10000),
+        'preprocessor__text_pipeline__tfidf__use_idf': (True, False),
+        'clf__n_estimators': [50, 100, 200],
+        'clf__min_samples_split': [2, 3, 4]
+    }
+
+    cv = GridSearchCV(pipeline, param_grid=parameters)
+
+    return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    """
+    Evaluates the trained model
+
+    Parameters:
+    model (sklearn model): the trained model
+    X_test (numpy array): An array with X values to test the model
+    Y_test (numpy array): An array with Y values to test the model
+    category_names (list): Names of the categories
+
+    """
+    Y_pred = model.predict(X_test)
+
+    for i, category in enumerate(category_names):
+        precision = precision_score(Y_test[i], Y_pred[i])
+        recall = recall_score(Y_test[i], Y_pred[i])
+        f1 = f1_score(Y_test[i], Y_pred[i])
+
+        print("", category)
+        print("Category: {} | F1-Score: {:.2f} % | Precision: {:.2f} % | Recall: {:.2f} %".format(category, 
+                                                                                                  f1*100, 
+                                                                                                  precision*100, 
+                                                                                                  recall*100))
 
 
 def save_model(model, model_filepath):
-    pass
+    """
+    Saves the trained model into a pickle file
+
+    Parameters: 
+    model (sklearn model): The trained model
+    model_filepath (string): The path to save the model
+    """
+    joblib.dump(model, model_filepath)
 
 
 def main():
